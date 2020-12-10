f79ffb670c927780859cb629463ec7c1bfcef462
xilareyna <68404722+xilareyna@users.noreply.github.com>
2020-12-09 20:24:21 -0600

    updating code

--- portfolio/server.js
+++ portfolio/server.js
@@ -1,4 +1,6 @@
-require("dotEnv").config();
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 
 //___________________
 //Dependencies
@@ -23,7 +25,7 @@ const PORT = process.env.PORT || 3001;
 //___________________
 // How to connect to the database either via heroku or locally
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/" + `project2`;
+  process.env[MONGODB_URI] || "mongodb://localhost/" + `project2`;
 
 console.log(process.env.MONGODB_URI);
 
